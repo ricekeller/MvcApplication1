fc2e72d3ea0e2b0e765361985ace58833f1e01bf
ricekeller <ricekeller@gmail.com>
2015-01-08 16:38:22 -0600

    t2
    t2

---
+++ Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+	public class AppUser
+	{
+		public string Id;
+		public string Name;
+		public string RegistrationId;
+	}
+}
---
+++ Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+	public enum MessageType
+	{
+		Text
+	}
+	public class Message
+	{
+		public AppUser Sender;
+		public AppUser Receiver;
+		private byte[] _message;
+		private MessageType _msgType;
+		public DateTime SendTimeStamp;
+	}
+}
